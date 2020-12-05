8daca9a4bac1a887a23d66fe718b93063e900b82
samimohammeden <74146854+samimohammeden@users.noreply.github.com>
2020-12-05 07:07:11 -0800

    Add files via upload

---
+++ my 5work.cpp
@@ -0,0 +1,152 @@
+//writeen by sami mohammeden hmed esmail
+//second class
+//department of computer
+#include <iostream>
+using namespace std;
+class linkedlist
+struct node{
+
+    int data;
+    node*next;
+};
+{
+void printLinkedlist(node*node)
+};
+int c=0
+while(node!=NULL)
+{
+    if(c>0)
+    {cout<<"_>"node_>data;
+    node =node_>next;
+    }};
+    class stack
+	{
+    	
+int top;
+puublic:
+	int[max];
+	stock(){top=-1;
+	}
+	bool push(int x);
+	int pop();
+	int peek();
+	bool is empty(stack)''	};{bool stack::push(int x)}
+	if(top>=(max-1)){cout<<"stock overflow";
+	return false;
+	}
+	else{
+		a[++top] =x;
+		cout<<x<<"pushed into stock\n";
+		return true;
+	}
+	if(top<0){
+		cout<<"stock is Empty";
+		return 0;
+	}
+	bool stack::is Empty(){return(top<0);
+};
+class  Queue
+}
+int front,end,length,queuearray[maxsize];
+public:
+Queue();
+{
+	front =0;
+	end=maxsize-1;
+	length=0;
+	
+	
+	}	
+	bool isempty()
+	{
+		return length ==0; 
+	}
+	bool isfull(){
+		return length=maxsize;
+	}
+	void addQueue(int number)
+	{
+		if(isfull())
+		{
+			cout<<"Queue is full";
+		}
+		else
+		{
+		end=(end+1)%maxsize;	
+		QueueArray[end] =number;
+		length++;
+			
+		}
+		
+		
+	}
+	void deleteQueue()
+	{
+		if(isEmpty)
+		{
+			cout<<"Queue is empty";
+			
+		}
+		else
+		{
+			front=(front+1)%maxsize;
+			length--;
+			
+		}
+	}
+			
+};
+int main() 
+{
+	class stack s;
+	s.push();
+	s.push();
+	s.push();
+	cout<< s.pop();
+
+cout<<"Enter no.of node="
+cin >>n;
+int num c=0;
+node*heade =new node; //head node
+node*temb =new node;      //temb node
+cin>>num;
+head->data=nuum;
+for(int i=2;i<=n;i++)
+if{c==0}
+
+}{
+cin>>num;
+temb_>data=nuum;
+head_>next=temb;
+}
+else{
+
+    cin>>num;
+    node*temb1 =new node;
+    temb1>data=num;
+    temb>next=temb1;    //point to temb1 to temb
+    temb=temb1;         //set temb as temb1
+}
+printLinkedlist(head);
+printstack()
+printQueue();}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
